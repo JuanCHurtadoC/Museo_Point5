1bce1022d58c46b23a3d944aca575413e9c1ec82
JcamiloHC <juanhurtado1126030@correo.itm.edu.co>
2024-11-17 11:31:09 -0500

    Se crean las clases del controlador

--- Museo/src/Controller/WorkOfArtController.java
+++ Museo/src/Controller/WorkOfArtController.java
@@ -1,9 +1,23 @@
 package Controller;
 
 import Dto_Generics.Generic;
+//import Model.Painting;
+//import Model.Portrait;
+//import Model.Sculpture;
 
 abstract class WorkOfArtController {
+  //  abstract boolean register(Painting objPainting, Portrait objPortrait, Sculpture objSculpture);
     
+    abstract <T> boolean register(Generic objT);
+
+    abstract String search(String titleArtwork);
     
+   // abstract boolean update(Painting objPainting, Portrait objPortrait, Sculpture objSculpture);
+
+    abstract <T> boolean update(String titleArtwork, Generic objT);
+
+    abstract boolean remove(String titleArtwork);
+
+    abstract String list();
     
 }
